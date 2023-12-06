200e0f53cda4a53ab6f54a08f17b6e9540b9397d
Florian Oberndörfer <florian8751t@gmail.com>
2023-12-06 13:56:38 +0100

    Assert No Null values

--- project/automatedTests.py
+++ project/automatedTests.py
@@ -8,18 +8,22 @@ def test_DataFrames():
     trainStations = automatedDataPipeline.getTrainStationsGroupedByPLZ()
     assert len(trainStations.columns) == (2)
     assert len(trainStations) > 0  # assert not empty
+    assert trainStations.isnull().values.any() == False # no null values
 
     areaInfos = automatedDataPipeline.getAreaInfos()
     assert len(areaInfos.columns) == (3)
     assert len(areaInfos) > 0  # assert not empty
+    assert areaInfos.isnull().values.any() == False # no null values
 
     allocation = automatedDataPipeline.getAllocation()
     assert len(allocation.columns) == (4)
     assert len(allocation) > 0  # assert not empty
+    assert allocation.isnull().values.any() == False # no null values
 
     cars = automatedDataPipeline.getCars()
     assert len(cars.columns) == (3)
     assert len(cars) > 0  # assert not empty
+    assert cars.isnull().values.any() == False # no null values
 
 
 def test_system():
@@ -30,6 +34,7 @@ def test_system():
     assert os.path.isfile(path) #check if file exists
     assert len(final.columns) == (8)
     assert len(final) > 0  # assert not empty
+    assert final.isnull().values.any() == False # no null values
     conn.close
 
 
